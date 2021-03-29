e7f66bd18331bc915712d0ce2c4b23d3a0876fcc
Paranthaman Pranavan <31650351+pranaone@users.noreply.github.com>
2021-03-29 09:37:26 +0530

    Initial

---
+++ src/app/payment-details/payment-details.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { PaymentDetail } from '../shared/payment-detail.model';
+import { PaymentDetailService } from '../shared/payment-detail.service';
+
+@Component({
+  selector: 'app-payment-details',
+  templateUrl: './payment-details.component.html',
+  styleUrls: ['./payment-details.component.css']
+})
+export class PaymentDetailsComponent implements OnInit {
+
+  constructor(public service: PaymentDetailService,
+    private toastr:ToastrService) { }
+
+  ngOnInit(): void {
+    this.service.refreshList();
+  }
+
+  populateForm(selectedRecord:PaymentDetail){
+    this.service.formData = Object.assign({},selectedRecord); //to avoid direct amendments
+  }
+
+  onDelete(id:number){
+    if(confirm('Are you sure to delete this record?')){
+      this.service.deletePaymentDetail(id).subscribe(
+        res => {
+          this.service.refreshList();
+          this.toastr.error('Deleted Successfully','Payment Detail Register');
+        },
+        err => {console.log(err)}
+      );
+    }
+ 
+  }
+
+}
